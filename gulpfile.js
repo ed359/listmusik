3af2b73c0d77de4ee49fdc88aa2b2e90d23de36d
ed359 <ewan.davies@gmail.com>
2016-02-08 17:42:29 +0000

    set up a dummy testing framework. Testing node-expat needs a big woraround

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
+var debug = require('gulp-debug');
 var del = require('del');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 var nwb = require('nw-builder');
+var path = require('path');
 var spawn = require('child_process').spawn;
 var spawnSync = require('child_process').spawnSync;
 
 var paths = {
   dist: 'dist',
-  nwcache: './dist/nwcache',
+  gulp: './node_modules/gulp/bin/gulp.js',
+  nw: {
+    bin: './node_modules/nw/bin/nw',
+    cache: './dist/nwcache',
+    package: './node_modules/nw/package.json'
+  },
   src: ['app/**/*'],
-  src_js: ['app/main.js', 'app/lib/*.js']
+  src_js: ['app/main.js', 'app/lib/*.js'],
+  test: 'test/*.js'
 };
 
 // INSTALLATION
@@ -25,7 +34,7 @@ gulp.task('postinstall', function(cb) {
   gutil.log('Installing node dependencies in app');
   spawnSync('npm', ['install'],
     { cwd: 'app', stdio: 'inherit' });
-  var nwpackage = require('./node_modules/nw/package.json');
+  var nwpackage = require(paths.nw.package);
   var nwtarget = nwpackage.version;
   gutil.log('Compiling node-expat for nw version', nwtarget);
 
@@ -40,11 +49,30 @@ gulp.task('postinstall', function(cb) {
 
 gulp.task('default', ['run']);
 
+gulp.task('test', function (cb) {
+  gutil.log("Please run $ gulp test-actual")
+  // return spawn(paths.nw.bin, [paths.gulp, '--gulpfile', 'gulpfile.js', 'test-actual'],
+  //   { cwd: '.', stdio: 'inherit' })
+  //   .on('close', cb);
+});
+
+gulp.task('test-actual', function () {
+  return gulp.src(paths.test, {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it 
+    .pipe(mocha({reporter: 'nyan'}));
+});
+
 gulp.task('lint', function() {
   return gulp.src(paths.src_js)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('run', ['lint'], function(cb) {
+  return spawn(paths.nw.bin, ['./app', '--debug'], { stdio: 'inherit' })
+    .on('close', cb);
+});
+gulp.task('start', ['run']);
 
 gulp.task('style', function() {
   return gulp.src(paths.src_js)
@@ -52,12 +80,6 @@ gulp.task('style', function() {
     .pipe(jscs.reporter())
     .pipe(jscs.reporter('fail'));
 });
-
-gulp.task('run', ['lint'], function(cb) {
-  return spawn('./node_modules/nw/bin/nw', ['./app', '--debug'], { stdio: 'inherit' })
-    .on('close', cb);
-});
-gulp.task('start', ['run']);
 
 // DEPLOY TASKS
 
@@ -70,7 +92,7 @@ gulp.task('deploy', ['lint', 'clean'], function() {
 
   var nw = new nwb({
     buildDir: paths.dist,
-    cacheDir: paths.nwcache,
+    cacheDir: paths.nw.cache,
     files: paths.src,
     // macCredits: 'path/to/credits.html'
     // macIcns: 'img/icon.icns',
